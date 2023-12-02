4ab921ab086b3833417814a3b3d73ddbec1e40f7
wrewsama <andrewlzs512@gmail.com>
2023-12-02 12:00:31 +0800

    add autoclosing keybinds for brackets and quotation marks

--- lua/configs/keybindings.lua
+++ lua/configs/keybindings.lua
@@ -11,3 +11,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- paste over something without resetting paste buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- auto close brackets and quotation marks
+vim.keymap.set("i", "{", "{}<Esc>ha")
+vim.keymap.set("i", "[", "[]<Esc>ha")
+vim.keymap.set("i", "(", "()<Esc>ha")
+vim.keymap.set("i", [["]], [[""<Esc>ha]])
+vim.keymap.set("i", [[']], [[''<Esc>ha]])
+vim.keymap.set("i", [[`]], [[``<Esc>ha]])
